1be0f058ce9feeebe5fefcf510b78b6cf5798738
Cole Savage <colesavage02@gmail.com>
2019-09-02 17:59:39 -0700

    Crossed off sdk migration from Idea Dump

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/calib/Idea_Dump.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/calib/Idea_Dump.java
@@ -3,7 +3,6 @@ package org.firstinspires.ftc.teamcode.util.calib;
 public class Idea_Dump {
     /**
      * TODO list
-     * 2. migrate to new sdk
      * 3. PID for other drive systems
      * 5. Kalman filter
      */
